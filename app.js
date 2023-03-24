eda00b1611dc31d0b8a1bc7e21c08afb03513f75
Amdrii <andreyk557@gmail.com>
2023-03-24 17:56:37 +0200

    feat[focusInput-func]

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const logo = document.querySelector("[data-logo]");
 
 const currentYear = new Date().getFullYear();
 for (let i = currentYear; i < currentYear + 10; i++) {
-  const option = document.querySelector("option");
+  const option = document.createElement("option");
   option.value = i;
   option.innerText = i;
   expirationSelect.append(option);
@@ -19,7 +19,7 @@ document.addEventListener("keydown", (e) => {
       if (input.selectionStart === 0 && input.selectionEnd === 0) {
         const prev = input.previousElementSibling;
         prev.focus();
-        prev.setSelectionStart = prev.value.length - 1;
+        prev.selectionStart = prev.value.length - 1;
         prev.selectionEnd = prev.value.length - 1;
         e.preventDefault();
       }
@@ -32,27 +32,73 @@ document.addEventListener("keydown", (e) => {
       ) {
         const next = input.nextElementSibling;
         next.focus();
-        next.setSelectionStart = 1;
+        next.selectionStart = 1;
         next.selectionEnd = 1;
+        e.preventDefault();
+      }
+      break;
+    }
+    case "Delete": {
+      if (
+        input.selectionStart === input.value.length &&
+        input.selectionEnd === input.value.length
+      ) {
+        const next = input.nextElementSibling;
+        next.value = next.value.substring(1, next.value.length);
+        next.focus();
+        next.selectionStart = 0;
+        next.selectionEnd = 0;
+        e.preventDefault();
+      }
+      break;
+    }
+    case "Backspace": {
+      if (input.selectionStart === 0 && input.selectionEnd === 0) {
+        const prev = input.previousElementSibling;
+        prev.value = prev.value.substring(0, prev.value.length - 1);
+        prev.focus();
+        prev.selectionStart = prev.value.length;
+        prev.selectionEnd = prev.value.length;
         e.preventDefault();
       }
       break;
     }
     default: {
       if (e.ctrlKey || e.altKey) return;
+      if (key.length > 1) return;
       if (key.match(/^[^0-9]$/)) return e.preventDefault();
 
       e.preventDefault();
-      onIputChange(input, key);
+      onInputChange(input, key);
     }
   }
 });
 
-function onIputChange(input, newValue) {
+document.addEventListener("paste", (e) => {
+  const input = e.target;
+  const data = e.clipboardData.getData("text");
+
+  if (!isConnectedInput(input)) return;
+  if (!data.match(/^[0-9]+$/)) return e.preventDefault();
+
+  e.preventDefault();
+  onInputChange(input, data);
+});
+
+function onInputChange(input, newValue) {
   const start = input.selectionStart;
   const end = input.selectionEnd;
   updateInputValue(input, newValue, start, end);
-  focusInput();
+  focusInput(input, newValue.length + start);
+  const firstFour = input
+    .closest("[data-connected-inputs]")
+    .querySelector("input").value;
+
+  if (firstFour.startsWith("4")) {
+    logo.src = "visa.svg";
+  } else if (firstFour.startsWith("5")) {
+    logo.src = "mastercard.svg";
+  }
 }
 
 function updateInputValue(input, extraValue, start = 0, end = 0) {
@@ -60,12 +106,26 @@ function updateInputValue(input, extraValue, start = 0, end = 0) {
     0,
     start
   )}${extraValue}${input.value.substring(end, 4)}`;
-  (input.value = newValue), substring(0, 4);
+  input.value = newValue.substring(0, 4);
   if (newValue > 4) {
     const next = input.nextElementSibling;
     if (next == null) return;
-    updateInputValue(next, newValue, substring(4));
+    updateInputValue(next, newValue.substring(4));
   }
+}
+
+function focusInput(input, dataLength) {
+  let addedChars = dataLength;
+  let currentInput = input;
+  while (addedChars > 4 && currentInput.nextElementSibling != null) {
+    addedChars -= 4;
+    currentInput = currentInput.nextElementSibling;
+  }
+  if (addedChars > 4) addedChars = 4;
+
+  currentInput.focus();
+  currentInput.selectionStart = addedChars;
+  currentInput.selectionEnd = addedChars;
 }
 
 function isConnectedInput(input) {
